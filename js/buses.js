2afe43049d4ead272a7a7ca1304a365b37cdfb27
John Montgomery <john@littlespikeyland.com>
2010-04-17 16:31:58 +0100

    added start of code to add a stop

--- js/buses.js
+++ js/buses.js
@@ -4,6 +4,21 @@ $(function() {
         $(back).removeClass('hidden');
     };
     
+    var busstop = function(element, name) {
+        var stop = {
+            element: element,
+            name: name,
+            
+            update: function() {
+                this.element.find('.stopname').text(this.name);
+            }
+        };
+        
+        return stop;
+    };
+    
+    var current_stop = null;
+    
     $('#front a.settings').click(function(event) {
         event.preventDefault();
         flip('#front', '#back');
@@ -11,6 +26,10 @@ $(function() {
     
     $('#back button.done').click(function(event) {
         event.preventDefault();
+        var name = $('#back form.add_stop input[name=stop_name]').val();
+        current_stop = busstop($('#front .details'), name);
+        current_stop.update();
         flip('#back', '#front');
     });
+    
 });
