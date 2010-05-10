a8236b774082eaa9e32fed5810171940e242c3c6
John Montgomery <john@littlespikeyland.com>
2010-05-10 22:13:55 +0100

    moved buses.db to root folder
    and script to generate it too

--- buses.py
+++ buses.py
@@ -5,6 +5,14 @@ import os.path
 ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
 STATIC_PATH = os.path.join(ROOT_PATH, 'static')
 
+DB_FILE = os.path.join(ROOT_PATH, 'buses.db')
+
+@route('/dyn/times/:stop')
+def times(stop):
+    pass
+
+# TODO need to set correct mimetype for manifest
+
 @route('/')
 @route('/(?P<filename>.*)')
 def static_files(filename='index.html'):
