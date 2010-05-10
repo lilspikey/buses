748063624553186e724dfc13fb36d77e014517e2
John Montgomery <john@littlespikeyland.com>
2010-05-10 22:28:14 +0100

    moved /dyn/search to bottle

--- buses.py
+++ buses.py
@@ -1,19 +1,57 @@
-from bottle import route, send_file, run
+from bottle import route, send_file, request, response, json_dumps, run
 
 import os.path
+import sqlite3 as db
 
 ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
 STATIC_PATH = os.path.join(ROOT_PATH, 'static')
 
 DB_FILE = os.path.join(ROOT_PATH, 'buses.db')
 
+def with_db_cursor(fn):
+    def _decorated(*arg, **kw):
+        conn = db.connect(DB_FILE)
+        try:
+            cursor = conn.cursor()
+            return fn(cursor, *arg, **kw)
+        finally:
+            conn.close()
+    return _decorated
+
+def escape_glob(glob):
+    import re
+    return re.sub(r'([\\\[\]?*])', r'\\\1', glob)
+
+def get_stops(cursor, q):
+    sql = 'select distinct name from stop'
+    sql_params = []
+    clauses = []
+    
+    if q:
+        clauses.append('name glob ? or naptan = ?')
+        esq = '%s*' % escape_glob(q)
+        sql_params.extend((esq, q))
+    
+    if clauses:
+        sql += (' where %s' % (' and '.join(clauses)))
+    
+    sql += ' order by name asc limit 30'
+    
+    stops = cursor.execute(sql, sql_params)
+    for (name,) in stops:
+        yield name
+
 @route('/dyn/times/:stop')
 def times(stop):
     pass
 
 @route('/dyn/search')
-def search():
-    pass
+@with_db_cursor
+def search(cursor):
+    q = request.GET.get('q', None)
+    response.content_type = 'application/json'
+    return json_dumps(list(get_stops(cursor, q)))
+    
 
 # TODO need to set correct mimetype for manifest
 
