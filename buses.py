c0a1c71777baa35683d30b869055ebeed808df67
John Montgomery <john@littlespikeyland.com>
2010-05-21 22:09:47 +0100

    start of work to get stops by distance

--- buses.py
+++ buses.py
@@ -7,15 +7,28 @@ from BeautifulSoup import BeautifulSoup
 import os.path
 import sqlite3 as db
 import urllib
+import re
+import math
 
 ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
 STATIC_PATH = os.path.join(ROOT_PATH, 'static')
 
 DB_FILE = os.path.join(ROOT_PATH, 'buses.db')
 
+def haversine(lat1, lng1, lat2, lng2):
+    print lat1, lng1, lat2, lng2
+    return 0
+#    (6371 * acos(cos(radians(38.7666667))
+#          * cos(radians(stop.lat))
+#          * cos(radians(stop.lng) - radians(-3.3833333))
+#          + sin(radians(38.7666667))
+#          * sin(radians(stop.lat))))
+
 def with_db_cursor(fn):
     def _decorated(*arg, **kw):
         conn = db.connect(DB_FILE)
+        
+        conn.create_function('haversine', 4, haversine)
         try:
             cursor = conn.cursor()
             return fn(cursor, *arg, **kw)
@@ -24,27 +37,40 @@ def with_db_cursor(fn):
     return _decorated
 
 def escape_glob(glob):
-    import re
     return re.sub(r'([\\\[\]?*])', r'\\\1', glob)
 
-def get_stops(cursor, q):
-    sql = 'select id, name from stop_name'
+def get_stops(cursor, q, ll):
+    fields = ['stop_name.id', 'stop_name.name', 'stop.lat', 'stop.lng']
+    clauses = ['stop_name.id = stop.name_id']
     sql_params = []
-    clauses = []
     
     if q:
         clauses.append('name glob ?')
         esq = '%s*' % escape_glob(q)
         sql_params.extend((esq,))
+    elif ll:
+        m = re.match(r'^(-?\d+\.?\d*),(-?\d+\.?\d*)$', ll)
+        if m:
+            lat, lng = m.groups()
+            lat, lng = float(lat), float(lng)
+            
+            fields.append('haversine(stop.lat, stop.lng, ?, ?) as distance')
+            sql_params.extend((lat, lng))
+    
+    sql = 'select %s from stop_name, stop' % (', '.join(fields))
     
     if clauses:
         sql += (' where %s' % (' and '.join(clauses)))
     
-    sql += ' order by name asc limit 30'
+    sql += ' order by name asc limit 60'
     
     stops = cursor.execute(sql, sql_params)
-    for (id, name) in stops:
-        yield { 'id': id, 'name': name }
+    for stop in stops:
+        id, name, lat, lng = stop[:4]
+        json = { 'id': id, 'name': name, 'lat': lat, 'lng': lng }
+        if len(stop) > 4:
+            json['distance'] = stop[4]
+        yield json
 
 @with_db_cursor
 def find_stop_name(cursor, name_id):
@@ -84,8 +110,9 @@ def times(name_id):
 @with_db_cursor
 def search(cursor):
     q = request.GET.get('q', None)
+    ll = request.GET.get('ll', None)
     response.content_type = 'application/json'
-    return json_dumps(list(get_stops(cursor, q)))
+    return json_dumps(list(get_stops(cursor, q, ll)))
     
 
 @route('/buses.manifest')
